ac157bc2f011149d13445847bd258db67ab6542f
Thomas Bergheim <knighthk@glvortex.net>
2024-06-28 16:01:35 +0200

    Add delete support

--- lib/phoenix_liveview_test/links/link.ex
+++ lib/phoenix_liveview_test/links/link.ex
@@ -1,5 +1,6 @@
 defmodule PhoenixLiveviewTest.Links.Link do
   use Ecto.Schema
+  require Regex
   import Ecto.Changeset
 
   schema "links" do
@@ -11,10 +12,28 @@ defmodule PhoenixLiveviewTest.Links.Link do
     timestamps(type: :utc_datetime)
   end
 
+  defp add_protocol(changeset) do
+    update_change(changeset, :url, fn url ->
+      case Regex.match?(~r/^https?:\/\//, url) do
+        true -> url
+        false -> "https://#{url}"
+      end
+    end)
+  end
+
+  defp foo(change) do
+    IO.inspect(change)
+    change
+  end
+
   @doc false
   def changeset(link, attrs \\ %{}) do
     link
     |> cast(attrs, [:url, :body, :user_id])
-    |> validate_required([:url, :body, :user_id])
+    |> validate_required([:url, :user_id])
+    |> validate_format(:url, ~r/.*\w\.\w/)
+    |> add_protocol()
+
+    # |> validate_format(:url, ~r/@/)
   end
 end
--- lib/phoenix_liveview_test_web/live/link_live/index.ex
+++ lib/phoenix_liveview_test_web/live/link_live/index.ex
@@ -16,6 +16,7 @@ defmodule PhoenixLiveviewTestWeb.LinkLive.Index do
     {:ok, socket}
   end
 
+  @impl Phoenix.LiveView
   def handle_event("submit", %{"link" => link_params}, socket) do
     params =
       link_params
@@ -42,4 +43,34 @@ defmodule PhoenixLiveviewTestWeb.LinkLive.Index do
         {:noreply, socket}
     end
   end
+
+  @impl Phoenix.LiveView
+  def handle_event("delete_link", %{"id" => id}, socket) do
+    user_id = socket.assigns.current_user.id
+    # TODO make sure we own this link!
+    link = Links.get_link!(id)
+
+    # Assuming Links.delete_link/1 is a helper defined in your Links context
+    {:ok, _} = Links.delete_link(link)
+
+    {:noreply,
+     socket
+     |> put_flash(:info, "Link deleted successfully")
+     |> assign(:links, Links.list_links(user_id))}
+  end
+
+  # def handle_event("validate", %{"link" => link_params}, socket) do
+  #   changeset = Links.Link.changeset(%Links.Link{})
+  #   IO.inspect(link_params)
+
+  #   socket =
+  #     socket
+  #     |> to_form(action: :validate)
+  #     |> assign(
+  #       :form,
+  #       to_form(changeset)
+  #     )
+
+  #   {:noreply, socket}
+  # end
 end
